d640778c098d0d51e7b2d53639d733d83a3c49f3
eneko98 <valeroeneko@gmail.com>
2019-05-15 12:41:32 +0200

    Arreglado el problema que habia con las clases .cpp .c y .h de las conexiones  en la Solución

--- JuegoProg4/JuegoProg4/Conexion.cpp
+++ JuegoProg4/JuegoProg4/Conexion.cpp
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <sqlite3.h>
+#include "sqlite3.h"
 #include <stdlib.h>
 #include "Conexion.h"
 
-
+sqlite3 *db;
+char *zErrMsg = 0;
 //Abrimos la BD, si no existe se crea una nueva.
 static void iniciarBD(int argc, char* argv[]) {
-	sqlite3 *db;
-	char *zErrMsg = 0;
 	int request;
-	
-
 	request = sqlite3_open("prog4db.db", &db);
 
 	if (request) {
@@ -43,8 +40,9 @@ static int llamada2(void *data, int argc, char **argv, char **azColName) {
 	printf("\n");
 	return 0;
 }
-static void crearTablas(int request, char *sql) {
-	
+static void crearTablas() {
+	int request;
+	const char *sql;
 	//Crear tablas
 	sql = "CREATE TABLE PERSONAJE("  \
 		"ID INT PRIMARY KEY NOT NULL," \
@@ -64,13 +62,15 @@ static void crearTablas(int request, char *sql) {
 	}
 }
 
-static void insertarDatos(int request, char *sql) {
+static void insertarDatos() {
+	int request;
+	const char *sql;
 	sql = "INSERT INTO PERSONAJE (ID,NAME,ENEMIGOS_ELIMINADOS,MONEDAS_RECOGIDAS) " \
-		"VALUES(1, 'P1', 3, 16);"
+		"VALUES(1, 'P1', 3, 16);";
 
 	request = sqlite3_exec(db, sql, llamada, 0, &zErrMsg);
 
-	if (rc != SQLITE_OK) {
+	if (request != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
 	}
@@ -80,12 +80,14 @@ static void insertarDatos(int request, char *sql) {
 
 }
 
-static void seleccionarDatos(int request, char *sql) {
-	const char* data = "Funcion llamada iniciada.";
+static void seleccionarDatos() {
+	int request;
+	const char *sql;
+	const char* data = "Funcion Llamada iniciada.";
 	sql = "SELECT * FROM PERSONAJE;";
 	request = sqlite3_exec(db, sql, llamada2, (void*)data, &zErrMsg);
 
-	if (rc != SQLITE_OK) {
+	if (request != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
 	}
@@ -94,12 +96,14 @@ static void seleccionarDatos(int request, char *sql) {
 	}
 
 }
-static void actualizarDatos(int request, char *sql) {
+static void actualizarDatos() {
+	int request;
+	char *sql;
 	const char* data = "Funcion llamada iniciada.";
 	//sql = 
 	request = sqlite3_exec(db, sql, llamada2, (void*)data, &zErrMsg);
 
-	if (rc != SQLITE_OK) {
+	if (request != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
 	}
@@ -108,12 +112,14 @@ static void actualizarDatos(int request, char *sql) {
 	}
 
 }
-static void borrarDatos(int request, char *sql) {
+static void borrarDatos() {
+	int request;
+	char *sql;
 	const char* data = "Funcion llamada iniciada.";
 	//sql = 
 	request = sqlite3_exec(db, sql, llamada2, (void*)data, &zErrMsg);
 
-	if (rc != SQLITE_OK) {
+	if (request != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
 	}
--- JuegoProg4/JuegoProg4/Conexion.h
+++ JuegoProg4/JuegoProg4/Conexion.h
@@ -8,8 +8,8 @@ typedef int(*sqlite3_callback)(
 static void iniciarBD(int argc, char* argv[]);
 static int llamada(void *NotUsed, int argc, char **argv, char **colName);
 static int llamada2(void *data, int argc, char **argv, char **azColName);
-static void crearTablas(int request, char *sql);
-static void insertarDatos(int request, char *sql);
-static void seleccionarDatos(int request, char *sql);
-static void actualizarDatos(int request, char *sql);
-static void borrarDatos(int request, char *sql);
+static void crearTablas();
+static void insertarDatos();
+static void seleccionarDatos();
+static void actualizarDatos();
+static void borrarDatos();
